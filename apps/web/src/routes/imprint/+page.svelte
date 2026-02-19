7e516d7bc2f19a563c2870e3813c326d5fae55f0
Kate <kate>
2026-02-19 09:09:46 +0100

    added: imprint button

--- apps/web/src/routes/imprint/+page.svelte
+++ apps/web/src/routes/imprint/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { marked } from 'marked';
 
   let content = '';
 
@@ -10,14 +9,13 @@
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-      const text = await response.text();
-      content = marked(text);
+      content = await response.text();
     } catch (error) {
-      console.error("Error:Couldn't fetch imprint data:", error);
-      content = "<p>Error: Imprint was not loaded.</p>";
+      console.error("Error couldn't load imprint:", error);
+      content = "Error: Imprint was not loaded.";
     }
   });
 </script>
 
 <h1>Imprint</h1>
-<div>{@html content}</div>
+<pre>{content}</pre>
