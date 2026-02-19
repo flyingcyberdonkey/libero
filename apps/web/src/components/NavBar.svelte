274ce599779a97bc1780e835a7cfd760482780e3
Kate <kate>
2026-02-19 09:06:54 +0100

    added: imprint button

--- apps/web/src/components/NavBar.svelte
+++ apps/web/src/components/NavBar.svelte
@@ -1,47 +1,44 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import settings from '../settings';
-	import authStore from '../auth';
-	import NavBar from 'components/DeprecatedNavBar/NavBar.svelte';
-	import Icon from 'components/Icon.svelte';
+  import { page } from '$app/state';
+  import settings from '../settings';
+  import authStore from '../auth';
+  import NavBar from 'components/DeprecatedNavBar/NavBar.svelte';
+  import Icon from 'components/Icon.svelte';
+  import NavBarButton from 'components/DeprecatedNavBar/NavBarButton.svelte';
+  import Logo from 'components/Logo.svelte';
+  import NavBarItem from 'components/DeprecatedNavBar/NavBarItem.svelte';
+  import NavBarButtonSet from 'components/DeprecatedNavBar/NavBarButtonSet.svelte';
 
-	import NavBarButton from 'components/DeprecatedNavBar/NavBarButton.svelte';
+  export let hasAuth = false;
+  export let repositoryURL = null;
 
-	import Logo from 'components/Logo.svelte';
-	import NavBarItem from 'components/DeprecatedNavBar/NavBarItem.svelte';
-
-	import NavBarButtonSet from 'components/DeprecatedNavBar/NavBarButtonSet.svelte';
-	export let hasAuth = false;
-	export let repositoryURL = null;
-
-	type WindowWithLogout = Window & {
-		_Logout: () => void;
-	};
-	const _Logout = () => (window as unknown as WindowWithLogout)._Logout();
-	let gistId = page.url.searchParams.get('gistId');
-	const homepageLink = `/course/${page.params?.courseName}/`;
-// if gistId is present, it will be added to the URL
-// ? `/course/${page.params.courseName}${gistId ? `?gistId=${gistId}` : ''}`
-//   : '/';
-
+  type WindowWithLogout = Window & {
+    _Logout: () => void;
+  };
+  const _Logout = () => (window as unknown as WindowWithLogout)._Logout();
+  let gistId = page.url.searchParams.get('gistId');
+  const homepageLink = `/course/${page.params?.courseName}/`;
 </script>
 
 <NavBar>
-	<div slot="left">
-		<Logo src="/images/logo.svg" alt="LibreLingo" link={homepageLink} />
-	</div>
+  <div slot="left">
+    <Logo src="/images/logo.svg" alt="LibreLingo" link={homepageLink} />
+  </div>
 
-	<div slot="right">
-		<NavBarButtonSet>
-			{#if repositoryURL}
-				<NavBarButton href={repositoryURL} target="_blank">Feedback</NavBarButton>
-			{/if}
-		</NavBarButtonSet>
-	</div>
+  <div slot="right">
+    <NavBarButtonSet>
+      {#if repositoryURL}
+        <NavBarButton href={repositoryURL} target="_blank">Feedback</NavBarButton>
+      {/if}
+      <!-- Korrigierte Links zu den Routen /imprint und /privacy -->
+      <NavBarButton href="/imprint">Imprint</NavBarButton>
+      <NavBarButton href="/privacy">Privacy</NavBarButton>
+    </NavBarButtonSet>
+  </div>
 </NavBar>
 
 <style>
-div {
-	height: 100%;
-}
+  div {
+    height: 100%;
+  }
 </style>
--- apps/web/src/routes/imprint/+page.svelte
+++ apps/web/src/routes/imprint/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { marked } from 'marked';
+
   let content = '';
 
   onMount(async () => {
-    const response = await fetch('/imprint.md');
-    const text = await response.text();
-    content = marked(text);
+    try {
+      const response = await fetch('/imprint.md');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const text = await response.text();
+      content = marked(text);
+    } catch (error) {
+      console.error("Error:Couldn't fetch imprint data:", error);
+      content = "<p>Error: Imprint was not loaded.</p>";
+    }
   });
 </script>
 
