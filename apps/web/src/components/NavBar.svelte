0282a1424c037b97d0f249e5caf95ad64f858682
Kate <kate>
2026-02-19 10:23:17 +0100

    added: navbar again after bugfixing courses

--- apps/web/src/components/NavBar.svelte
+++ apps/web/src/components/NavBar.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
+  import { page } from '$app/state';
   import NavBar from 'components/DeprecatedNavBar/NavBar.svelte';
   import NavBarButton from 'components/DeprecatedNavBar/NavBarButton.svelte';
-  import Logo from 'components/Logo.svelte';
   import NavBarButtonSet from 'components/DeprecatedNavBar/NavBarButtonSet.svelte';
+  import Logo from 'components/Logo.svelte';
+
+  export let hasAuth = false;
+  export let repositoryURL = null;
+
+  let courseName = page.params?.courseName || '';
+  const homepageLink = `/course/${courseName}`;
 </script>
 
 <NavBar>
   <div slot="left">
-    <Logo src="/images/logo.svg" alt="LibreLingo" link="/" />
+    <Logo src="/images/logo.svg" alt="LibreLingo" link={homepageLink} />
   </div>
+
   <div slot="right">
     <NavBarButtonSet>
-      <!-- Temporär keine Links zu Imprint/Privacy -->
+      {#if repositoryURL}
+        <NavBarButton href={repositoryURL} target="_blank">Course Feedback</NavBarButton>
+      {/if}
+      <!-- Links zu Imprint und Privacy (ohne .md) -->
+      <NavBarButton href="/imprint">Imprint</NavBarButton>
+      <NavBarButton href="/privacy">Privacy</NavBarButton>
     </NavBarButtonSet>
   </div>
 </NavBar>
+
+<style>
+  div {
+    height: 100%;
+  }
+</style>
